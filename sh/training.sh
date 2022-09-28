42511b7730a1ddb4a78bae295ba9b75ad6221234
jpedrofontes <jpedrofontes8@gmail.com>
2022-09-28 11:05:48 +0100

    Training script and gitignore changes

---
+++ sh/training.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#SBATCH --job-name=JP_PHENOTYPE_CLASSIFIER_CUSTOM
+#SBATCH --time=1000:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=compute
+#SBATCH --nodelist=vision1
+#SBATCH --gres=gpu:2
+#SBATCH --mem=122000
+#SBATCH --cpus-per-task=32
+
+source /home/mguevaral/jpedro/phenotype-classifier/venv/bin/activate
+module load CUDA
+module load cuDNN
+
+python /home/mguevaral/jpedro/phenotype-classifier/train_and_evaluate.py 0
